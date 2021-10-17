68848cef1d1f0dcc7c701f98c378de7cecd57a2d
iHux3 <ihux3x@gmail.com>
2021-10-17 20:04:04 +0200

    deleted parts related to btc claiming

---
+++ contracts/GlobalsAndUtility.sol
@@ -0,0 +1,560 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract GlobalsAndUtility {
+
+    /*  DailyDataUpdate   (auto-generated event)
+
+        uint40            timestamp       -->  data0 [ 39:  0]
+        uint16            beginDay        -->  data0 [ 55: 40]
+        uint16            endDay          -->  data0 [ 71: 56]
+        bool              isAutoUpdate    -->  data0 [ 79: 72]
+        address  indexed  updaterAddr
+    */
+    event DailyDataUpdate(
+        uint256 data0,
+        address indexed updaterAddr
+    );
+
+    /*  StakeStart        (auto-generated event)
+
+        uint40            timestamp       -->  data0 [ 39:  0]
+        address  indexed  stakerAddr
+        uint40   indexed  stakeId
+        uint72            stakedHearts    -->  data0 [111: 40]
+        uint72            stakeShares     -->  data0 [183:112]
+        uint16            stakedDays      -->  data0 [199:184]
+    */
+    event StakeStart(
+        uint256 data0,
+        address indexed stakerAddr,
+        uint40 indexed stakeId
+    );
+
+    /*  StakeGoodAccounting(auto-generated event)
+
+        uint40            timestamp       -->  data0 [ 39:  0]
+        address  indexed  stakerAddr
+        uint40   indexed  stakeId
+        uint72            stakedHearts    -->  data0 [111: 40]
+        uint72            stakeShares     -->  data0 [183:112]
+        uint72            payout          -->  data0 [255:184]
+        uint72            penalty         -->  data1 [ 71:  0]
+        address  indexed  senderAddr
+    */
+    event StakeGoodAccounting(
+        uint256 data0,
+        uint256 data1,
+        address indexed stakerAddr,
+        uint40 indexed stakeId,
+        address indexed senderAddr
+    );
+
+    /*  StakeEnd          (auto-generated event)
+
+        uint40            timestamp       -->  data0 [ 39:  0]
+        address  indexed  stakerAddr
+        uint40   indexed  stakeId
+        uint72            stakedHearts    -->  data0 [111: 40]
+        uint72            stakeShares     -->  data0 [183:112]
+        uint72            payout          -->  data0 [255:184]
+        uint72            penalty         -->  data1 [ 71:  0]
+        uint16            servedDays      -->  data1 [ 87: 72]
+        bool              prevUnlocked    -->  data1 [ 95: 88]
+    */
+    event StakeEnd(
+        uint256 data0,
+        uint256 data1,
+        address indexed stakerAddr,
+        uint40 indexed stakeId
+    );
+
+    /*  ShareRateChange   (auto-generated event)
+
+        uint40            timestamp       -->  data0 [ 39:  0]
+        uint40            shareRate       -->  data0 [ 79: 40]
+        uint40   indexed  stakeId
+    */
+    event ShareRateChange(
+        uint256 data0,
+        uint40 indexed stakeId
+    );
+
+    /* Origin address */
+    address internal constant ORIGIN_ADDR = 0x9A6a414D6F3497c05E3b1De90520765fA1E07c03;
+
+    /* ERC20 constants */
+    string public constant name = "HEX";
+    string public constant symbol = "HEX";
+    uint8 public constant decimals = 8;
+
+    /* Hearts per Satoshi = 10,000 * 1e8 / 1e8 = 1e4 */
+    uint256 private constant HEARTS_PER_HEX = 10 ** uint256(decimals); // 1e8
+    uint256 private constant HEX_PER_BTC = 1e4;
+    uint256 private constant SATOSHIS_PER_BTC = 1e8;
+    uint256 internal constant HEARTS_PER_SATOSHI = HEARTS_PER_HEX / SATOSHIS_PER_BTC * HEX_PER_BTC;
+
+    /* Time of contract launch (2019-12-03T00:00:00Z) */
+    uint256 internal constant LAUNCH_TIME = 1575331200;
+
+    /* Size of a Hearts or Shares uint */
+    uint256 internal constant HEART_UINT_SIZE = 72;
+
+    /* Stake timing parameters */
+    uint256 internal constant MIN_STAKE_DAYS = 1;
+    uint256 internal constant MIN_AUTO_STAKE_DAYS = 350;
+
+    uint256 internal constant MAX_STAKE_DAYS = 5555; // Approx 15 years
+
+    uint256 internal constant EARLY_PENALTY_MIN_DAYS = 90;
+
+    uint256 private constant LATE_PENALTY_GRACE_WEEKS = 2;
+    uint256 internal constant LATE_PENALTY_GRACE_DAYS = LATE_PENALTY_GRACE_WEEKS * 7;
+
+    uint256 private constant LATE_PENALTY_SCALE_WEEKS = 100;
+    uint256 internal constant LATE_PENALTY_SCALE_DAYS = LATE_PENALTY_SCALE_WEEKS * 7;
+
+    /* Stake shares Longer Pays Better bonus constants used by _stakeStartBonusHearts() */
+    uint256 private constant LPB_BONUS_PERCENT = 20;
+    uint256 private constant LPB_BONUS_MAX_PERCENT = 200;
+    uint256 internal constant LPB = 364 * 100 / LPB_BONUS_PERCENT;
+    uint256 internal constant LPB_MAX_DAYS = LPB * LPB_BONUS_MAX_PERCENT / 100;
+
+    /* Stake shares Bigger Pays Better bonus constants used by _stakeStartBonusHearts() */
+    uint256 private constant BPB_BONUS_PERCENT = 10;
+    uint256 private constant BPB_MAX_HEX = 150 * 1e6;
+    uint256 internal constant BPB_MAX_HEARTS = BPB_MAX_HEX * HEARTS_PER_HEX;
+    uint256 internal constant BPB = BPB_MAX_HEARTS * 100 / BPB_BONUS_PERCENT;
+
+    /* Share rate is scaled to increase precision */
+    uint256 internal constant SHARE_RATE_SCALE = 1e5;
+
+    /* Share rate max (after scaling) */
+    uint256 internal constant SHARE_RATE_UINT_SIZE = 40;
+    uint256 internal constant SHARE_RATE_MAX = (1 << SHARE_RATE_UINT_SIZE) - 1;
+
+    /* Globals expanded for memory (except _latestStakeId) and compact for storage */
+    struct GlobalsCache {
+        // 1
+        uint256 _lockedHeartsTotal;
+        uint256 _nextStakeSharesTotal;
+        uint256 _shareRate;
+        uint256 _stakePenaltyTotal;
+        // 2
+        uint256 _dailyDataCount;
+        uint256 _stakeSharesTotal;
+        uint40 _latestStakeId;
+        //
+        uint256 _currentDay;
+    }
+
+    struct GlobalsStore {
+        // 1
+        uint72 lockedHeartsTotal;
+        uint72 nextStakeSharesTotal;
+        uint40 shareRate;
+        uint72 stakePenaltyTotal;
+        // 2
+        uint16 dailyDataCount;
+        uint72 stakeSharesTotal;
+        uint40 latestStakeId;
+    }
+
+    GlobalsStore public globals;
+
+    /* Daily data */
+    struct DailyDataStore {
+        uint72 dayPayoutTotal;
+        uint72 dayStakeSharesTotal;
+    }
+
+    mapping(uint256 => DailyDataStore) public dailyData;
+
+    /* Stake expanded for memory (except _stakeId) and compact for storage */
+    struct StakeCache {
+        uint40 _stakeId;
+        uint256 _stakedHearts;
+        uint256 _stakeShares;
+        uint256 _lockedDay;
+        uint256 _stakedDays;
+        uint256 _unlockedDay;
+    }
+
+    struct StakeStore {
+        uint40 stakeId;
+        uint72 stakedHearts;
+        uint72 stakeShares;
+        uint16 lockedDay;
+        uint16 stakedDays;
+        uint16 unlockedDay;
+    }
+
+    mapping(address => StakeStore[]) public stakeLists;
+
+    /* Temporary state for calculating daily rounds */
+    struct DailyRoundState {
+        uint256 _allocSupplyCached;
+        uint256 _payoutTotal;
+    }
+
+    /**
+     * @dev PUBLIC FACING: Optionally update daily data for a smaller
+     * range to reduce gas cost for a subsequent operation
+     * @param beforeDay Only update days before this day number (optional; 0 for current day)
+     */
+    function dailyDataUpdate(uint256 beforeDay)
+        external
+    {
+        GlobalsCache memory g;
+        GlobalsCache memory gSnapshot;
+        _globalsLoad(g, gSnapshot);
+
+        if (beforeDay != 0) {
+            require(beforeDay <= g._currentDay, "HEX: beforeDay cannot be in the future");
+
+            _dailyDataUpdate(g, beforeDay, false);
+        } else {
+            /* Default to updating before current day */
+            _dailyDataUpdate(g, g._currentDay, false);
+        }
+
+        _globalsSync(g, gSnapshot);
+    }
+
+    /**
+     * @dev PUBLIC FACING: External helper to return multiple values of daily data with
+     * a single call. Ugly implementation due to limitations of the standard ABI encoder.
+     * @param beginDay First day of data range
+     * @param endDay Last day (non-inclusive) of data range
+     * @return list Fixed array of packed values
+     */
+    function dailyDataRange(uint256 beginDay, uint256 endDay)
+        external
+        view
+        returns (uint256[] memory list)
+    {
+        require(beginDay < endDay && endDay <= globals.dailyDataCount, "HEX: range invalid");
+
+        list = new uint256[](endDay - beginDay);
+
+        uint256 src = beginDay;
+        uint256 dst = 0;
+        uint256 v;
+        do {
+            v = uint256(dailyData[src].dayStakeSharesTotal) << HEART_UINT_SIZE;
+            v |= uint256(dailyData[src].dayPayoutTotal);
+
+            list[dst++] = v;
+        } while (++src < endDay);
+
+        return list;
+    }
+
+    /**
+     * @dev PUBLIC FACING: External helper to return most global info with a single call.
+     * Ugly implementation due to limitations of the standard ABI encoder.
+     * @return Fixed array of values
+     */
+    function globalInfo()
+        external
+        view
+        returns (uint256[9] memory)
+    {
+        return [
+            // 1
+            globals.lockedHeartsTotal,
+            globals.nextStakeSharesTotal,
+            globals.shareRate,
+            globals.stakePenaltyTotal,
+            // 2
+            globals.dailyDataCount,
+            globals.stakeSharesTotal,
+            globals.latestStakeId,
+            //
+            block.timestamp,
+            totalSupply()
+        ];
+    }
+
+    function totalSupply() public view returns (uint) {
+        return 0;
+    }
+
+    /**
+     * @dev PUBLIC FACING: ERC20 totalSupply() is the circulating supply and does not include any
+     * staked Hearts. allocatedSupply() includes both.
+     * @return Allocated Supply in Hearts
+     */
+    function allocatedSupply()
+        external
+        view
+        returns (uint256)
+    {
+        return totalSupply() + globals.lockedHeartsTotal;
+    }
+
+    /**
+     * @dev PUBLIC FACING: External helper for the current day number since launch time
+     * @return Current day number (zero-based)
+     */
+    function currentDay()
+        external
+        view
+        returns (uint256)
+    {
+        return _currentDay();
+    }
+
+    function _currentDay()
+        internal
+        view
+        returns (uint256)
+    {
+        return (block.timestamp - LAUNCH_TIME) / 1 days;
+    }
+
+    function _dailyDataUpdateAuto(GlobalsCache memory g)
+        internal
+    {
+        _dailyDataUpdate(g, g._currentDay, true);
+    }
+
+    function _globalsLoad(GlobalsCache memory g, GlobalsCache memory gSnapshot)
+        internal
+        view
+    {
+        // 1
+        g._lockedHeartsTotal = globals.lockedHeartsTotal;
+        g._nextStakeSharesTotal = globals.nextStakeSharesTotal;
+        g._shareRate = globals.shareRate;
+        g._stakePenaltyTotal = globals.stakePenaltyTotal;
+        // 2
+        g._dailyDataCount = globals.dailyDataCount;
+        g._stakeSharesTotal = globals.stakeSharesTotal;
+        g._latestStakeId = globals.latestStakeId;
+        
+        g._currentDay = _currentDay();
+
+        _globalsCacheSnapshot(g, gSnapshot);
+    }
+
+    function _globalsCacheSnapshot(GlobalsCache memory g, GlobalsCache memory gSnapshot)
+        internal
+        pure
+    {
+        // 1
+        gSnapshot._lockedHeartsTotal = g._lockedHeartsTotal;
+        gSnapshot._nextStakeSharesTotal = g._nextStakeSharesTotal;
+        gSnapshot._shareRate = g._shareRate;
+        gSnapshot._stakePenaltyTotal = g._stakePenaltyTotal;
+        // 2
+        gSnapshot._dailyDataCount = g._dailyDataCount;
+        gSnapshot._stakeSharesTotal = g._stakeSharesTotal;
+        gSnapshot._latestStakeId = g._latestStakeId;
+    }
+
+    function _globalsSync(GlobalsCache memory g, GlobalsCache memory gSnapshot)
+        internal
+    {
+        if (g._lockedHeartsTotal != gSnapshot._lockedHeartsTotal
+            || g._nextStakeSharesTotal != gSnapshot._nextStakeSharesTotal
+            || g._shareRate != gSnapshot._shareRate
+            || g._stakePenaltyTotal != gSnapshot._stakePenaltyTotal) {
+            // 1
+            globals.lockedHeartsTotal = uint72(g._lockedHeartsTotal);
+            globals.nextStakeSharesTotal = uint72(g._nextStakeSharesTotal);
+            globals.shareRate = uint40(g._shareRate);
+            globals.stakePenaltyTotal = uint72(g._stakePenaltyTotal);
+        }
+        if (g._dailyDataCount != gSnapshot._dailyDataCount
+            || g._stakeSharesTotal != gSnapshot._stakeSharesTotal
+            || g._latestStakeId != gSnapshot._latestStakeId) {
+            // 2
+            globals.dailyDataCount = uint16(g._dailyDataCount);
+            globals.stakeSharesTotal = uint72(g._stakeSharesTotal);
+            globals.latestStakeId = g._latestStakeId;
+        }
+    }
+
+    function _stakeLoad(StakeStore storage stRef, uint40 stakeIdParam, StakeCache memory st)
+        internal
+        view
+    {
+        /* Ensure caller's stakeIndex is still current */
+        require(stakeIdParam == stRef.stakeId, "HEX: stakeIdParam not in stake");
+
+        st._stakeId = stRef.stakeId;
+        st._stakedHearts = stRef.stakedHearts;
+        st._stakeShares = stRef.stakeShares;
+        st._lockedDay = stRef.lockedDay;
+        st._stakedDays = stRef.stakedDays;
+        st._unlockedDay = stRef.unlockedDay;
+    }
+
+    function _stakeUpdate(StakeStore storage stRef, StakeCache memory st)
+        internal
+    {
+        stRef.stakeId = st._stakeId;
+        stRef.stakedHearts = uint72(st._stakedHearts);
+        stRef.stakeShares = uint72(st._stakeShares);
+        stRef.lockedDay = uint16(st._lockedDay);
+        stRef.stakedDays = uint16(st._stakedDays);
+        stRef.unlockedDay = uint16(st._unlockedDay);
+    }
+
+    function _stakeAdd(
+        StakeStore[] storage stakeListRef,
+        uint40 newStakeId,
+        uint256 newStakedHearts,
+        uint256 newStakeShares,
+        uint256 newLockedDay,
+        uint256 newStakedDays
+    )
+        internal
+    {
+        stakeListRef.push(
+            StakeStore(
+                newStakeId,
+                uint72(newStakedHearts),
+                uint72(newStakeShares),
+                uint16(newLockedDay),
+                uint16(newStakedDays),
+                uint16(0) // unlockedDay
+            )
+        );
+    }
+
+    /**
+     * @dev Efficiently delete from an unordered array by moving the last element
+     * to the "hole" and reducing the array length. Can change the order of the list
+     * and invalidate previously held indexes.
+     * @notice stakeListRef length and stakeIndex are already ensured valid in stakeEnd()
+     * @param stakeListRef Reference to stakeLists[stakerAddr] array in storage
+     * @param stakeIndex Index of the element to delete
+     */
+    function _stakeRemove(StakeStore[] storage stakeListRef, uint256 stakeIndex)
+        internal
+    {
+        uint256 lastIndex = stakeListRef.length - 1;
+
+        /* Skip the copy if element to be removed is already the last element */
+        if (stakeIndex != lastIndex) {
+            /* Copy last element to the requested element's "hole" */
+            stakeListRef[stakeIndex] = stakeListRef[lastIndex];
+        }
+
+        /*
+            Reduce the array length now that the array is contiguous.
+            Surprisingly, 'pop()' uses less gas than 'stakeListRef.length = lastIndex'
+        */
+        stakeListRef.pop();
+    }
+
+    /**
+     * @dev Estimate the stake payout for an incomplete day
+     * @param g Cache of stored globals
+     * @param stakeSharesParam Param from stake to calculate bonuses for
+     * @param day Day to calculate bonuses for
+     * @return payout Payout in Hearts
+     */
+    function _estimatePayoutRewardsDay(GlobalsCache memory g, uint256 stakeSharesParam, uint256 day)
+        internal
+        view
+        returns (uint256 payout)
+    {
+        /* Prevent updating state for this estimation */
+        GlobalsCache memory gTmp;
+        _globalsCacheSnapshot(g, gTmp);
+
+        DailyRoundState memory rs;
+        rs._allocSupplyCached = totalSupply() + g._lockedHeartsTotal;
+
+        _dailyRoundCalc(gTmp, rs, day);
+
+        /* Stake is no longer locked so it must be added to total as if it were */
+        gTmp._stakeSharesTotal += stakeSharesParam;
+
+        payout = rs._payoutTotal * stakeSharesParam / gTmp._stakeSharesTotal;
+
+        return payout;
+    }
+
+    function _dailyRoundCalc(GlobalsCache memory g, DailyRoundState memory rs, uint256 day)
+        private
+        pure
+    {
+        /*
+            Calculate payout round
+
+            Inflation of 3.69% inflation per 364 days             (approx 1 year)
+            dailyInterestRate   = exp(log(1 + 3.69%)  / 364) - 1
+                                = exp(log(1 + 0.0369) / 364) - 1
+                                = exp(log(1.0369) / 364) - 1
+                                = 0.000099553011616349            (approx)
+
+            payout  = allocSupply * dailyInterestRate
+                    = allocSupply / (1 / dailyInterestRate)
+                    = allocSupply / (1 / 0.000099553011616349)
+                    = allocSupply / 10044.899534066692            (approx)
+                    = allocSupply * 10000 / 100448995             (* 10000/10000 for int precision)
+        */
+        rs._payoutTotal = rs._allocSupplyCached * 10000 / 100448995;
+
+        if (g._stakePenaltyTotal != 0) {
+            rs._payoutTotal += g._stakePenaltyTotal;
+            g._stakePenaltyTotal = 0;
+        }
+    }
+
+    function _dailyRoundCalcAndStore(GlobalsCache memory g, DailyRoundState memory rs, uint256 day)
+        private
+    {
+        _dailyRoundCalc(g, rs, day);
+
+        dailyData[day].dayPayoutTotal = uint72(rs._payoutTotal);
+        dailyData[day].dayStakeSharesTotal = uint72(g._stakeSharesTotal);
+    }
+
+    function _dailyDataUpdate(GlobalsCache memory g, uint256 beforeDay, bool isAutoUpdate)
+        private
+    {
+        if (g._dailyDataCount >= beforeDay) {
+            /* Already up-to-date */
+            return;
+        }
+
+        DailyRoundState memory rs;
+        rs._allocSupplyCached = totalSupply() + g._lockedHeartsTotal;
+
+        uint256 day = g._dailyDataCount;
+
+        _dailyRoundCalcAndStore(g, rs, day);
+
+        /* Stakes started during this day are added to the total the next day */
+        if (g._nextStakeSharesTotal != 0) {
+            g._stakeSharesTotal += g._nextStakeSharesTotal;
+            g._nextStakeSharesTotal = 0;
+        }
+
+        while (++day < beforeDay) {
+            _dailyRoundCalcAndStore(g, rs, day);
+        }
+
+        _emitDailyDataUpdate(g._dailyDataCount, day, isAutoUpdate);
+        g._dailyDataCount = day;
+    }
+
+    function _emitDailyDataUpdate(uint256 beginDay, uint256 endDay, bool isAutoUpdate)
+        private
+    {
+        emit DailyDataUpdate( // (auto-generated event)
+            uint256(uint40(block.timestamp))
+                | (uint256(uint16(beginDay)) << 40)
+                | (uint256(uint16(endDay)) << 56)
+                | (isAutoUpdate ? (1 << 72) : 0),
+            msg.sender
+        );
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("hardhat-gas-reporter");
+require("hardhat-abi-exporter");
+
+module.exports = {
+    solidity: {
+        version: "0.8.4",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    networks: {
+        hardhat: {
+            forking: {
+                url: `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+            }
+        },
+        kovan: {
+            url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+            gasPrice: 1e9
+        },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+        },
+    },
+    abiExporter: {
+        path: './abi',
+        clear: true,
+        flat: true,
+    }
+};
